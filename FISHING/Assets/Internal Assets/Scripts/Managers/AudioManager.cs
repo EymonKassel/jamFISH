ecd28cdf2f748f401559c7001077299d032f58e6
EymonKassel <55111120+EymonKassel@users.noreply.github.com>
2024-09-14 11:36:08 +0300

    AudioManager system + minor fixes

--- FISHING/Assets/Internal Assets/Scripts/Managers/AudioManager.cs
+++ FISHING/Assets/Internal Assets/Scripts/Managers/AudioManager.cs
@@ -1,9 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
+    [Header("Audio Sources")]
+    [SerializeField]
+    public AudioSource MusicSource;
+    [SerializeField]
+    public AudioSource SFXSource;
+
+    [Header("Audio Clips")]
+    public AudioClip BackgroundMusic;
+    public AudioClip TakeItem;
+    public AudioClip DropItem;
+    public AudioClip SpawItem;
+
     private void Awake() {
         DontDestroyOnLoad(gameObject);
     }
+    private void Start() {
+        MusicSource.clip = BackgroundMusic;
+        MusicSource.Play();
+    }
+    public void PlaySFX(AudioClip clip) {
+        SFXSource.PlayOneShot(clip);
+    }
+
 }
--- FISHING/Assets/Internal Assets/Scripts/Managers/GameManager.cs
+++ FISHING/Assets/Internal Assets/Scripts/Managers/GameManager.cs
@@ -1,27 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     [SerializeField]
-    private float holdTime = 2f; 
-    private float holdTimer = 0f; 
+    private float _holdTime = 2f; 
+    private float _holdTimer = 0f; 
 
     private void Awake() {
         DontDestroyOnLoad(gameObject);
     }
 
     private void Update() {
-        if ( Input.GetMouseButton(2) ) { 
-            holdTimer += Time.deltaTime; 
+        RestartGame();
+    }
+    private void RestartGame() {
+        if ( Input.GetMouseButton(2) ) {
+            _holdTimer += Time.deltaTime;
 
-            if ( holdTimer >= holdTime ) {
+            if ( _holdTimer >= _holdTime ) {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-                holdTimer = 0f;
+                _holdTimer = 0f;
             }
         } else if ( Input.GetMouseButtonUp(2) ) {
-            holdTimer = 0f;
+            _holdTimer = 0f;
         }
     }
 }
